e469a36215d9afce4a5a89e8c74f6d469169ec56
Bob den Otter <bob@twokings.nl>
2017-05-31 20:48:44 +0200

    Initial commit 🚀

---
+++ src/SimpleStatsExtension.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Local\Simplestats;
+
+use Bolt\Extension\SimpleExtension;
+use Bolt\Menu\MenuEntry;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SimpleStatsExtension extends SimpleExtension
+{
+    /**
+     * Set up the default configuration.
+     *
+     * @see https://docs.bolt.cm/extensions/basics/configuration#providing-defaults
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return [
+            'foo' => 'bar',
+            'qux' => 'baz'
+        ];
+    }
+
+    /**
+     * Add a backend menu entry under 'extensions'.
+     *
+     * @see https://docs.bolt.cm/extensions/intermediate/admin-menus#registering-menu-entries
+     *
+     * @return array
+     */
+    protected function registerMenuEntries()
+    {
+        $menu = new MenuEntry('SimpleStats-menu', 'simplestats');
+        $menu->setLabel('SimpleStats')
+            ->setIcon('fa:bar-chart')
+            ->setPermission('dashboard')
+        ;
+
+        return [
+            $menu,
+        ];
+    }
+
+
+    /**
+     * Register routes for the Backend
+     *
+     * @see https://docs.bolt.cm/extensions/intermediate/controllers-routes#route-callback-functions
+     *
+     * @param ControllerCollection $collection
+     */
+    protected function registerBackendRoutes(ControllerCollection $collection)
+    {
+        // GET requests on the /bolt/extensions/foo route
+        $collection->get('/extensions/simplestats', [$this, 'backendSimpleStats']);
+
+    }
+
+    /**
+     * Register twig functions to be used in templates.
+     *
+     * @see https://docs.bolt.cm/extensions/basics/twig#registering-twig-functions
+     *
+     * @return array
+     */
+    protected function registerTwigFunctions()
+    {
+        return [
+            'foo' => 'fooFunction',
+            'bar' => ['barFunction', ['is_safe' => ['html']]]
+        ];
+    }
+
+
+    /**
+     * Render and return the Twig file templates/foo.twig
+     *
+     * @return string
+     */
+    public function fooFunction()
+    {
+        return $this->renderTemplate('foo.twig');
+    }
+
+
+    /**
+     * Controller - Callback function for backend route
+     *
+     * @param Application $app
+     * @param Request     $request
+     *
+     * @return Response
+     */
+    public function backendSimpleStats(Application $app, Request $request)
+    {
+        return $this->renderTemplate('simplestats.twig');
+    }
+}
